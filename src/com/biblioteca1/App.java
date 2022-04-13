1e1ba80d5a62cf6bed8fbaa80fa9ab39dd67dcf8
keii25 <kevin.martinezc@hotmail.com>
2022-04-13 12:03:08 -0500

    update class App

--- src/com/biblioteca1/App.java
+++ src/com/biblioteca1/App.java
@@ -7,53 +7,71 @@ package com.biblioteca1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Library songLibrary = new Library();
+        Scanner read = new Scanner(System.in);
+        int year;
+        String gender;
+
 
         songLibrary.playlist.add(songLibrary.color);
         songLibrary.playlist.add(songLibrary.me_Llamas);
         songLibrary.playlist.add(songLibrary.todo_Para_mi);
+        songLibrary.playlist.add(songLibrary.volver_a_Respirar);
+        songLibrary.playlist.add(songLibrary.sin_Ti);
+        songLibrary.playlist.add(songLibrary.paso_a_Paso);
+
+        System.out.println("\n");
+        System.out.println("------------------------------");
+        System.out.println("My Playlist\n");
+
+        int count = songLibrary.playlist.size();
+        for (Song song : songLibrary.playlist) {
+            System.out.println("Song: " + song);
+        }
 
 
         List<Song> songFiltered = new ArrayList<>();
+        System.out.println("\n");
+        System.out.println("""
+                --- Gender ---
+                - Bachata
+                - Pop
+                - Reggaeton
+                """);
+        System.out.println("\n");
+        System.out.print("Enter the gender of the song to filter: ");
+        gender = read.nextLine();
         for(Song song : songLibrary.playlist){
-            if(song.getGender().equals("Bachata")){  //gender = ['Bachata', 'Pop', 'Tropipop']
+            if(song.getGender().equals(gender)){  //gender = ['Bachata', 'Pop', 'Tropipop']
                 songFiltered.add(song);
             }
         }
 
         List<Song> songFiltered2 = new ArrayList<>();
+        System.out.print("Enter the year of the song to filter: ");
+        year = read.nextInt();
         for(Song song : songLibrary.playlist){
-            if(song.getDate().equals(2020)){  // anios = [2016, 2017, 2018, 2019, 2020]
+            if(song.getDate().equals(year)){  // anios = [2009, 2016, 2017, 2018, 2019, 2020]
                 songFiltered2.add(song);
             }
         }
 
         System.out.println("\n");
-        System.out.println("Filtered by Gender: ");
-        for (Song value : songFiltered) {
-            System.out.println("> " + value);
-        }
 
         System.out.println("\n");
-        System.out.println("Filtered by year: ");
-        for (Song song : songFiltered2) {
+        System.out.println("Filtered by Gender: " + gender);
+        for (Song song : songFiltered) {
             System.out.println("> " + song);
         }
 
-
-        /*
         System.out.println("\n");
-        System.out.println("------------------------------");
-        System.out.println("My Playlist\n");
-
-        int count = playlist.size();
-        for (Song song : playlist) {
-            System.out.println("Cancion: " + song);
+        System.out.println("Filtered by year: "+ year);
+        for (Song song : songFiltered2) {
+            System.out.println("> " + song);
         }
-        System.out.println("\n");
-        * */
     }
 }
--- src/com/biblioteca1/Library.java
+++ src/com/biblioteca1/Library.java
@@ -12,7 +12,6 @@ public class Library {
             1,
             2020);
 
-
     Song me_Llamas = new Song("Me Llamas", "Pop",
             "gilbertoDaza.png",
             "Album Daza",
@@ -26,4 +25,22 @@ public class Library {
             3.10,
             3,
             2020);
+    Song paso_a_Paso = new Song("Paso a Paso", "Pop",
+            "vaes.png",
+            "Album Espejo",
+            3.53,
+            4,
+            2019);
+    Song sin_Ti = new Song("Sin ti", "Reggaeton",
+            "alexZurdo.png",
+            "Album Quien contra Nosotros",
+            4.24,
+            5,
+            2018);
+    Song volver_a_Respirar = new Song("Volver a Respirar", "Pop",
+            "davidScarpeta.png",
+            "Album Volver a Respirar",
+            3.56,
+            6,
+            2009);
 }
